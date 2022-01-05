050ed62e01550ef85c4a0857095a9f2256a533aa
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2022-01-05 20:56:52 +0700

    Added styled button

---
+++ src/components/Buttons/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+ 
+const FixedButton = styled.button`
+    position: fixed;
+    right: 1%;
+    top: 20%;
+    background-color: var(--baby-blue);
+    outline: none;
+    border: none;
+    min-width: 100px;
+`
+
+export { FixedButton }
